c4133a18ca014827597cffc62b7b4f742eb6a6fe
antoniojunior52 <antoniocarlosjuninhojuca@gmail.com>
2023-10-10 12:15:57 -0300

    Learning_StringFunction_JS

---
+++ index.js
@@ -0,0 +1,54 @@
+let umaString = "O rato roeu a roupa do rei de roma.";
+
+
+//========Exibir caracter=======================
+// alert(umaString[7]); 
+// alert(umaString.charAt(4));
+// console.log(umaString.indexOf('i', 7));
+// console.log(umaString.lastIndexOf('i', 7));
+// console.log(umaString[7]);
+// console.log(umaString.charAt(4));
+// =============================================
+
+
+//=====Concatenar/Adicionar palavras===========
+// console.log(umaString.concat(' oi'));
+// console.log(umaString + " oi")
+// console.log(`${umaString} oi`)
+//=============================================
+
+
+//======Encontrar Caracter utilizando expressão regular===========
+// console.log(umaString.match(/[a-z]/g));
+//console.log(umaString.search(/[p]/));
+//================================================================
+
+
+//========Utilizar replace para alterar um elemento da string==========
+// console.log(umaString.replace('r', 'l')); Substitui uma letra
+// console.log(umaString.replace(/r/g, 'l')); Substitui todas letra
+//=====================================================================
+
+
+//========Utilizar o Length para descobrir a quantidade de caracteres existentes na string========
+// console.log(umaString.length);
+// console.log(umaString.slice(2,6));
+// console.log(umaString.slice(-3));
+// console.log(umaString.slice(32));
+// console.log(umaString.length -3);
+// console.log(umaString.slice(-5, umaString - 1));
+// console.log(umaString.slice(-5, -1));
+// console.log(umaString.substring(umaString.length -5, umaString.length -1));
+//================================================================================================
+
+
+//==========Utilizando o split no array para separar e mostrar um caracter específico(Valor não incluído)====
+// console.log(umaString.split('r'));
+// console.log(umaString.split(' ', 4));
+//===========================================================================================================
+
+
+// //===========Transformar caracter em maiúsculo=========
+// console.log(umaString.toUpperCase());
+// console.log(umaString.toLowerCase());
+// //=====================================================
